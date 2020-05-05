1828e5e1c7f754c374183d1b35f7ce999e15232b
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2020-05-05 02:23:55 +0100

    fixed bug causing error log in the chart

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -7,7 +7,7 @@ const Chart = ({ close, graphData, countryName }) => {
         <div>
             <button onClick={close}>close</button>
             <ResponsiveContainer width='100%' height={450}>
-                <LineChart width='100%' height={400} data={graphData} margin={{ top: 5, right: 15, bottom: 5, left: 15 }}>
+                <LineChart height={400} data={graphData} margin={{ top: 5, right: 15, bottom: 5, left: 15 }}>
                     <Line type="monotone" dataKey="confirmed" dot={false} stroke="red" />
                     <Line type="monotone" dataKey="deaths" dot={false} stroke="black" />
                     <Line type="monotone" dataKey="recovered" dot={false} stroke="green" />
